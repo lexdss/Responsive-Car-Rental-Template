119415619f4c25ca9460c7a0b716e9ae8a557b70
Anon <lexdss@gmail.com>
2017-02-15 17:20:01 +0200

    category and add car page

---
+++ js/script.js
@@ -0,0 +1,29 @@
+(function($){
+
+	$.fn.picker = function(a){
+		
+		$('html').on('click', function(e){
+			alert('dfdf');
+		})
+
+	}
+
+})(jQuery);
+
+/*$(document).ready(function(){
+
+	function DatePiker(){
+		alert('N');	
+			$('.tumblr').toggleClass('tumblr-p');
+			if( !$('.tumblr').hasClass('tumblr-p') ){
+				$('i.calendar').after('<div class="tumblr">sdfsdf</div>');
+			}else{
+				$('.tumblr').remove();
+			}
+
+
+	}
+
+
+
+});*/
